408a8bb82f1bbe615a5e52cb6970665f94222822
angeloesaki <angeloesaki@gmail.com>
2020-06-01 16:25:59 +0900

    正誤数をカウントする処理を追加

--- js/main.js
+++ js/main.js
@@ -7,22 +7,33 @@
   //↓は打つべき文字のインデックス
   let loc = 0;
 
+  //正解の数を管理するための変数
+  let score = 0;
+  //不正解の数を管理するための変数
+  let miss = 0;
+
   const target = document.getElementById("target");
+  const scoreLabel = document.getElementById("score");
+  const missLabel = document.getElementById("miss");
 
   target.textContent = word;
 
-  //タイプしたキーを取得
+  //e.keyでタイプしたキーを取得
   window.addEventListener("keydown", (e) => {
-    console.log(e.key);
+    // console.log(e.key);
     //タイピングの正誤判定
     //もしタイプされた文字とwordのloc番目の文字が同じなら
     if (e.key === word[loc]) {
       //scoreを表示し、locの数字を１増やす
-      console.log("score");
+      // console.log("score");
       loc++;
+      score++;
+      scoreLabel.textContent = score;
     } else {
       //間違っているならmissを表示
-      console.log("miss");
+      // console.log("miss");
+      miss++;
+      missLabel.textContent = miss;
     }
   });
 }
