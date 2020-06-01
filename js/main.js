276679f82103d4a7484ffd089df17fd061b4fb6b
angeloesaki <angeloesaki@gmail.com>
2020-06-01 17:10:15 +0900

    正解した文字の表示を変更する処理を追加

--- js/main.js
+++ js/main.js
@@ -18,6 +18,23 @@
 
   target.textContent = word;
 
+  //正解した文字の表示を変更する処理
+  function updateTarget() {
+    //_を格納していくための変数
+    let placeholder = "";
+
+    //locのインデックスの一個前までplaceholderに_を足していく
+    for (let i = 0; i < loc; i++) {
+      placeholder += "_";
+    }
+
+    //_と残りの文字を表示
+    //残りの文字＝wordのインデックスがloc番目以降の文字を表示
+    //部分文字列を取得できるsubstring()を使用
+    //substring()の引数には表示したい文字列のスタート地点のインデックスを当てる
+    target.textContent = placeholder + word.substring(loc);
+  }
+
   //e.keyでタイプしたキーを取得
   window.addEventListener("keydown", (e) => {
     // console.log(e.key);
@@ -27,6 +44,7 @@
       //scoreを表示し、locの数字を１増やす
       // console.log("score");
       loc++;
+      updateTarget();
       score++;
       scoreLabel.textContent = score;
     } else {
